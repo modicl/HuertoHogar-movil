03dd277bf344ff231cb4c2293b635f26d1f5065f
modicl <fe.villarroelb@duocuc.cl>
2025-11-30 17:17:05 -0300

    UPDATE : ICONO APP

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.example.huertohogarapp"
         minSdk = 33
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
